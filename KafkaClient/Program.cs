91e41f5bfcbb5d7bd8c3eeed28f350aa93fe710d
edgar <contacto@edgargutierrez.com.mx>
2022-06-28 17:01:11 -0500

    Adding code

---
+++ KafkaClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace KafkaClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            Console.WriteLine("Start");
+            Consume("NurseCreatedEvent", config);
+
+
+        }
+        static void Consume(string topic, ClientConfig config)
+        {
+            var consumerConfig = new ConsumerConfig(config);
+            consumerConfig.GroupId = "dotnet-example-group-1";
+            consumerConfig.AutoOffsetReset = AutoOffsetReset.Earliest;
+            consumerConfig.EnableAutoCommit = false;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build())
+            {
+                consumer.Subscribe(new string[] { "NurseCreatedEvent", "NurseDispatchedvent", "NurseUpdatedEvent", "NurseReleasedEvent" });
+                
+                try
+                {
+                    while (true)
+                    {
+                        var cr = consumer.Consume(cts.Token);
+                        Console.WriteLine($"Consumed record with key {cr.Message.Key} and value {cr.Message.Value}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ctrl-C was pressed.
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
